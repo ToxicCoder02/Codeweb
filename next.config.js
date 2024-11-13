7857c5c4f70f3f07d491ef5b153ed86e19f0755d
Rohan Upendra Patil <89687802+ToxicCoder02@users.noreply.github.com>
2024-11-13 12:47:20 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 // next.config.js
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   output: 'export',
-  basePath: 'Codeweb ',
-  assetPrefix: 'Codeweb ',
+  basePath: isProd ? '/Codeweb' : '',
+  assetPrefix: isProd ? '/Codeweb/' : '',
 };
